245c6efc8f62bc766380c1b5e6ff52e949c7d511
Marion <mshadbolt@users.noreply.github.com>
2024-03-11 14:06:51 -0700

    remove randomness, change to DONOR_000

--- src/json_to_csv.py
+++ src/json_to_csv.py
@@ -9,7 +9,6 @@ import json
 import pandas as pd
 import argparse
 from pathlib import Path
-import random
 
 
 def process_donor(df):
@@ -57,7 +56,10 @@ def process_exposure(df):
 
 def process_followups(df):
     rows = df.shape[0]
-    bool_list = random.choices([True, False], k=rows)
+    half_length = int(rows/2)
+    bool_list = [True, False] * half_length
+    if len(bool_list) < rows:
+        bool_list.append(True)
     df.loc[bool_list, 'submitter_treatment_id'] = ""
     reverse_list = [not x for x in bool_list]
     df.loc[reverse_list, 'submitter_primary_diagnosis_id'] = ""
@@ -65,7 +67,7 @@ def process_followups(df):
 
 
 def add_extra_donors():
-    donor_11 = {
+    donor_011 = {
             "cause_of_death": "Unknown",
             "date_of_death": {
                 "month_interval": 90,
@@ -78,166 +80,166 @@ def add_extra_donors():
             ],
             "program_id": "SYNTHETIC-2",
             "sex_at_birth": "Other",
-            "submitter_donor_id": "DONOR_11"
+            "submitter_donor_id": "DONOR_011"
         }
     # the null donor
-    donor_12 = {
-            "submitter_donor_id": "DONOR_12",
+    donor_000 = {
+            "submitter_donor_id": "DONOR_000",
             "program_id": "SYNTHETIC-2"
         }
-    return donor_11, donor_12
+    return donor_011, donor_000
 
 
 def add_pds():
-    donor_12 = [{
+    donor_000 = [{
         "submitter_primary_diagnosis_id": "PRIMARY_DIAGNOSIS_1a",
-        "submitter_donor_id": "DONOR_12",
+        "submitter_donor_id": "DONOR_000",
         "program_id": "SYNTHETIC-2"
     }]
-    return donor_12
+    return donor_000
 
 
 def add_specimens():
-    donor_12 = [{
+    donor_000 = [{
         "submitter_specimen_id": "SPECIMEN_1a",
-        "submitter_donor_id": "DONOR_12",
+        "submitter_donor_id": "DONOR_000",
         "submitter_primary_diagnosis_id": "PRIMARY_DIAGNOSIS_1a",
         "program_id": "SYNTHETIC-2"
     }]
-    return donor_12
+    return donor_000
 
 
 def add_samples():
-    donor_12 = [
+    donor_000 = [
         {
             "submitter_sample_id": "SAMPLE_REGISTRATION_1a",
             "submitter_specimen_id": "SPECIMEN_1a",
-            "submitter_donor_id": "DONOR_12",
+            "submitter_donor_id": "DONOR_000",
             "program_id": "SYNTHETIC-2"
         },
         {
             "submitter_sample_id": "SAMPLE_REGISTRATION_2a",
             "submitter_specimen_id": "SPECIMEN_1a",
-            "submitter_donor_id": "DONOR_12",
+            "submitter_donor_id": "DONOR_000",
             "program_id": "SYNTHETIC-2"
         }
     ]
-    return donor_12
+    return donor_000
 
 
 def add_treatments():
-    donor_12 = [
+    donor_000 = [
         {
             "submitter_treatment_id": "TREATMENT_1a",
-            "submitter_donor_id": "DONOR_12",
+            "submitter_donor_id": "DONOR_000",
             "submitter_primary_diagnosis_id": "PRIMARY_DIAGNOSIS_1a",
             "program_id": "SYNTHETIC-2"
         },
         {
             "submitter_treatment_id": "TREATMENT_2a",
-            "submitter_donor_id": "DONOR_12",
+            "submitter_donor_id": "DONOR_000",
             "submitter_primary_diagnosis_id": "PRIMARY_DIAGNOSIS_1a",
             "program_id": "SYNTHETIC-2"
         }
     ]
-    return donor_12
+    return donor_000
 
 
 def add_chemo():
-    donor_12 = [
+    donor_000 = [
         {
             "submitter_treatment_id": "TREATMENT_2a",
             "actual_cumulative_drug_dose": 60,
-            "submitter_donor_id": "DONOR_12",
+            "submitter_donor_id": "DONOR_000",
             "program_id": "SYNTHETIC-2"
         },
         {
             "submitter_treatment_id": "TREATMENT_2a",
             "prescribed_cumulative_drug_dose": 4600,
-            "submitter_donor_id": "DONOR_12",
+            "submitter_donor_id": "DONOR_000",
             "program_id": "SYNTHETIC-2"
         },
         {
             "submitter_treatment_id": "TREATMENT_1a",
             "drug_name": "Gemcitabine",
             "drug_reference_database": "NCI Thesaurus",
-            "submitter_donor_id": "DONOR_12",
+            "submitter_donor_id": "DONOR_000",
             "program_id": "SYNTHETIC-2"
         }
     ]
-    return donor_12
+    return donor_000
 
 
 def add_followups():
-    donor_12 = [
+    donor_000 = [
         {
             "submitter_follow_up_id": "FOLLOW_UP_2a",
             "submitter_treatment_id": "TREATMENT_1a",
-            "submitter_donor_id": "DONOR_12",
+            "submitter_donor_id": "DONOR_000",
             "program_id": "SYNTHETIC-2"
         },
         {
             "submitter_follow_up_id": "FOLLOW_UP_1a",
             "submitter_primary_diagnosis_id": "PRIMARY_DIAGNOSIS_1a",
-            "submitter_donor_id": "DONOR_12",
+            "submitter_donor_id": "DONOR_000",
             "program_id": "SYNTHETIC-2"
         }
     ]
-    return donor_12
+    return donor_000
 
 
 def add_comorbidities():
-    donor_12 = [{
+    donor_000 = [{
         "age_at_comorbidity_diagnosis": 56,
         "comorbidity_type_code": "C43.9",
-        "submitter_donor_id": "DONOR_12",
+        "submitter_donor_id": "DONOR_000",
         "program_id": "SYNTHETIC-2"
     },
         {
             "comorbidity_treatment": "Photodynamic therapy",
-            "submitter_donor_id": "DONOR_12",
+            "submitter_donor_id": "DONOR_000",
             "program_id": "SYNTHETIC-2"
         },
         {
             "prior_malignancy": "Yes",
-            "submitter_donor_id": "DONOR_12",
+            "submitter_donor_id": "DONOR_000",
             "program_id": "SYNTHETIC-2"
         }
 
     ]
-    return donor_12
+    return donor_000
 
 
 def add_exposures():
-    donor_12 = [
+    donor_000 = [
         {
             "tobacco_smoking_status": "Current smoker",
-            "submitter_donor_id": "DONOR_12",
+            "submitter_donor_id": "DONOR_000",
             "program_id": "SYNTHETIC-2"
         },
         {
             "pack_years_smoked": 72,
-            "submitter_donor_id": "DONOR_12",
+            "submitter_donor_id": "DONOR_000",
             "program_id": "SYNTHETIC-2"
         }
     ]
-    return donor_12
+    return donor_000
 
 
 def add_biomarkers():
-    donor_12 = [
+    donor_000 = [
         {
             "test_date": {
                 "month_interval": 49,
                 "day_interval": 1470,
 
             },
-            "submitter_donor_id": "DONOR_12",
+            "submitter_donor_id": "DONOR_000",
             "program_id": "SYNTHETIC-2"
         },
         {
             "ca125": 109,
-            "submitter_donor_id": "DONOR_12",
+            "submitter_donor_id": "DONOR_000",
             "program_id": "SYNTHETIC-2"
         },
         {
@@ -246,57 +248,57 @@ def add_biomarkers():
                 "HPV58",
                 "HPV35"
             ],
-            "submitter_donor_id": "DONOR_12",
+            "submitter_donor_id": "DONOR_000",
             "program_id": "SYNTHETIC-2"
         },
         {
             "er_status": "Not applicable",
             "her2_ihc_status": "Cannot be determined",
             "her2_ish_status": "Positive",
-            "submitter_donor_id": "DONOR_12",
+            "submitter_donor_id": "DONOR_000",
             "program_id": "SYNTHETIC-2"
         },
         {
             "cea": 5,
-            "submitter_donor_id": "DONOR_12",
+            "submitter_donor_id": "DONOR_000",
             "program_id": "SYNTHETIC-2"
         }
     ]
-    return donor_12
+    return donor_000
 
 
 def add_hormone_therapies():
-    donor_12 = [
+    donor_000 = [
         {
             "submitter_treatment_id": "TREATMENT_2a",
             "drug_name": "Cabergoline",
-            "submitter_donor_id": "DONOR_12",
+            "submitter_donor_id": "DONOR_000",
             "program_id": "SYNTHETIC-2"
         },
         {
             "submitter_treatment_id": "TREATMENT_2a",
             "drug_name": "Lutetium Lu 177 Dotatate",
             "drug_reference_identifier": "557845",
-            "submitter_donor_id": "DONOR_12",
+            "submitter_donor_id": "DONOR_000",
             "program_id": "SYNTHETIC-2"
         },
         {
             "submitter_treatment_id": "TREATMENT_1a",
             "prescribed_cumulative_drug_dose": 106,
             "actual_cumulative_drug_dose": 85,
-            "submitter_donor_id": "DONOR_12",
+            "submitter_donor_id": "DONOR_000",
             "program_id": "SYNTHETIC-2"
         }
     ]
-    return donor_12
+    return donor_000
 
 
 def add_immunotherapies():
-    donor_12 = [
+    donor_000 = [
         {
             "submitter_treatment_id": "TREATMENT_2a",
             "immunotherapy_type": "Other immunomodulatory substances",
-            "submitter_donor_id": "DONOR_12",
+            "submitter_donor_id": "DONOR_000",
             "program_id": "SYNTHETIC-2"
         },
         {
@@ -304,68 +306,68 @@ def add_immunotherapies():
             "drug_name": "Nivolumab",
             "drug_reference_identifier": "987654",
             "drug_reference_database": "PubChem",
-            "submitter_donor_id": "DONOR_12",
+            "submitter_donor_id": "DONOR_000",
             "program_id": "SYNTHETIC-2"
         },
         {
             "submitter_treatment_id": "TREATMENT_1a",
             "immunotherapy_drug_dose_units": "mg/kg",
-            "submitter_donor_id": "DONOR_12",
+            "submitter_donor_id": "DONOR_000",
             "program_id": "SYNTHETIC-2"
         }
     ]
-    return donor_12
+    return donor_000
 
 
 def add_radiations():
-    donor_12 = [
+    donor_000 = [
         {
             "submitter_treatment_id": "TREATMENT_2a",
             "radiation_therapy_type": "Internal",
-            "submitter_donor_id": "DONOR_12",
+            "submitter_donor_id": "DONOR_000",
             "program_id": "SYNTHETIC-2"
         },
         {
             "submitter_treatment_id": "TREATMENT_2a",
             "radiation_therapy_modality": "Teleradiotherapy neutrons (procedure)",
-            "submitter_donor_id": "DONOR_12",
+            "submitter_donor_id": "DONOR_000",
             "program_id": "SYNTHETIC-2"
         },
         {
             "submitter_treatment_id": "TREATMENT_1a",
             "anatomical_site_irradiated": "Right Maxilla",
-            "submitter_donor_id": "DONOR_12",
+            "submitter_donor_id": "DONOR_000",
             "program_id": "SYNTHETIC-2"
         }
     ]
-    return donor_12
+    return donor_000
 
 
 def add_surgeries():
-    donor_12 = [
+    donor_000 = [
         {
             "submitter_treatment_id": "TREATMENT_2a",
             "submitter_specimen_id": "SPECIMEN_1a",
             "surgery_type": "Wide Local Excision",
-            "submitter_donor_id": "DONOR_12",
+            "submitter_donor_id": "DONOR_000",
             "program_id": "SYNTHETIC-2"
         },
         {
             "submitter_treatment_id": "TREATMENT_2a",
             "submitter_specimen_id": "SPECIMEN_1a",
             "surgery_site": "C11",
-            "submitter_donor_id": "DONOR_12",
+            "submitter_donor_id": "DONOR_000",
             "program_id": "SYNTHETIC-2"
         },
         {
             "submitter_treatment_id": "TREATMENT_1a",
             "submitter_specimen_id": "SPECIMEN_1a",
             "surgery_location": "Metastatic",
-            "submitter_donor_id": "DONOR_12",
+            "submitter_donor_id": "DONOR_000",
             "program_id": "SYNTHETIC-2"
         }
     ]
-    return donor_12
+    return donor_000
 
 
 def add_objects(filename):
@@ -410,8 +412,10 @@ def convert_to_csv(size, input_path):
     Path(output_dir).mkdir(parents=True, exist_ok=True)
 
     # Iterate through all JSON files in the synthetic_data_folder
+    print(f"Processing json files from {synthetic_data_folder}...")
     for filename in os.listdir(synthetic_data_folder):
         if filename.endswith('.json') and not filename.startswith("Program"):
+            print(filename)
             json_file_path = os.path.join(synthetic_data_folder, filename)
             with open(json_file_path, 'r') as f:
                 data = json.load(f)
@@ -436,6 +440,7 @@ def convert_to_csv(size, input_path):
                 # Save the DataFrame to a CSV file in the output directory
                 output_csv_file = os.path.join(output_dir, f"{filename.replace('.json', '.csv')}")
                 df.to_csv(output_csv_file, index=False)
+    print("All done!")
 
 
 def parse_args():
